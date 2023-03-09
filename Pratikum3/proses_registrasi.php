0d444ebfa5958543b116d57bc381b4377aa4a801
Putra Rafly Pratama <putraraflypratama@gmail.com>
2023-03-09 15:31:43 +0700

    upload praktik 3

---
+++ Pratikum3/proses_registrasi.php
@@ -0,0 +1,20 @@
+<?php
+
+$domisili = ["Jakarta","Bogor","Depok","Tanggerang","Bekasi"];
+$program_studi = [
+    "SI"=>"Sistem Informasi",
+    "TI"=>"Teknin Informatika",
+    "BD"=>"Bisnis Digital"
+];
+$skillls = [
+    "HTML"=>10,
+    "CSS"=>10,
+    "Javascript"=>20,
+    "RWDBOOSTRAP"=>20,
+    "PHP"=>30,
+    "Python"=>30,
+    "Java"=>50,
+];
+
+?>
+
